95abd25b94a696c1858810d742803153c208e480
Sedat Enes <121612670+sedattcakir@users.noreply.github.com>
2026-08-13 02:15:26 +0300

    Birleştirme sonrası TypeScript hatalarını düzelt

---
+++ cloudflare-env.d.ts
@@ -0,0 +1,17 @@
+declare module "cloudflare:workers" {
+  export const env: {
+    DB?: D1Database;
+    [key: string]: unknown;
+  };
+}
+
+interface Fetcher {
+  fetch(input: RequestInfo | URL, init?: RequestInit): Promise<Response>;
+}
+
+interface D1Database {
+  prepare(query: string): any;
+  batch<T = unknown>(statements: any[]): Promise<T[]>;
+  exec(query: string): Promise<any>;
+  dump(): Promise<ArrayBuffer>;
+}
