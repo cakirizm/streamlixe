61024f9892c40021a40db4700b69ef13c1a5b623
cakirizm <cakir.dubai@gmail.com>
2026-08-20 00:17:11 +0400

    fix(tv): remove unsupported remote key handler

--- android/app/src/main/java/com/streamlivex/android/tv/TvRoot.kt
+++ android/app/src/main/java/com/streamlivex/android/tv/TvRoot.kt
@@ -23,8 +23,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.key.nativeKeyEvent
-import androidx.compose.ui.input.key.onPreviewKeyEvent
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
