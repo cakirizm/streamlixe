1f6f2f517a03ebdd0153b7399e50736b96df6586
cakirizm <cakir.dubai@gmail.com>
2026-08-20 00:40:37 +0400

    update livetvscreen

--- android/app/src/main/java/com/streamlivex/android/tv/live/LiveTvScreen.kt
+++ android/app/src/main/java/com/streamlivex/android/tv/live/LiveTvScreen.kt
@@ -5,6 +5,7 @@ import android.os.Looper
 import android.view.KeyEvent
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
