38b828b23632143e771cfb25a20504516ced688a
cakirizm <cakir.dubai@gmail.com>
2026-08-20 00:33:56 +0400

    fix(tv): replace incompatible TV key handlers

--- android/app/src/main/java/com/streamlivex/android/tv/live/LiveTvScreen.kt
+++ android/app/src/main/java/com/streamlivex/android/tv/live/LiveTvScreen.kt
@@ -32,7 +32,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.key.nativeKeyEvent
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
